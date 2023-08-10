ce80ad9f9f8a1014b12f2016be99a594bd20cd0f
Guru Prasad Mohapatra <smartters.studio@gmail.com>
2023-08-10 11:28:52 +0700

    API CALL

--- lib/home/profile_widget.dart
+++ lib/home/profile_widget.dart
@@ -1,7 +1,23 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class ProfileWidget extends StatelessWidget {
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ProfileWidget extends StatefulWidget {
   const ProfileWidget({Key? key}) : super(key: key);
+
+  @override
+  State<ProfileWidget> createState() => _ProfileWidgetState();
+}
+
+class _ProfileWidgetState extends State<ProfileWidget> {
+  Future<Response> getProfile() async {
+    var pref = await SharedPreferences.getInstance();
+    var id = pref.getString('login');
+    var dio = Dio();
+    return dio.get('http://148.72.158.178/~nandigho/blog/profile.php?id=$id');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,78 +26,91 @@ class ProfileWidget extends StatelessWidget {
 
     return Container(
       color: Colors.blue[50],
-      child: Stack(
-        children: [
-          Positioned(
-            top: (height - 150) / 2 -
-                kToolbarHeight -
-                kBottomNavigationBarHeight -
-                32,
-            right: (width / 2) - 125 - 32,
-            child: Card(
-              margin: EdgeInsets.zero,
-              elevation: 8,
-              child: Padding(
-                padding: const EdgeInsets.all(32.0),
-                child: Container(
-                  height: 150,
-                  width: 250,
-                  child: Center(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "name",
-                          style: TextStyle(color: Colors.grey),
+      child: FutureBuilder<Response>(
+        future: getProfile(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var jsonResponse = jsonDecode(snapshot.data!.data);
+
+            return Stack(
+              children: [
+                Positioned(
+                  top: (height - 150) / 2 -
+                      kToolbarHeight -
+                      kBottomNavigationBarHeight -
+                      32,
+                  right: (width / 2) - 125 - 32,
+                  child: Card(
+                    margin: EdgeInsets.zero,
+                    elevation: 8,
+                    child: Padding(
+                      padding: const EdgeInsets.all(32.0),
+                      child: Container(
+                        height: 150,
+                        width: 250,
+                        child: Center(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "name",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                              SizedBox(height: 4),
+                              Text(
+                                jsonResponse['name'],
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
+                              SizedBox(height: 12),
+                              Text(
+                                "Email",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                              SizedBox(height: 4),
+                              Text(
+                                jsonResponse['email'],
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
+                              SizedBox(height: 12),
+                              Text(
+                                "Phone",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                              SizedBox(height: 4),
+                              Text(
+                                jsonResponse['phone'],
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
+                            ],
+                          ),
                         ),
-                        SizedBox(height: 4),
-                        Text(
-                          "Guru Prasad Mohapatra",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 16),
-                        ),
-                        SizedBox(height: 12),
-                        Text(
-                          "Email",
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          "guru@mail.com",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 16),
-                        ),
-                        SizedBox(height: 12),
-                        Text(
-                          "Phone",
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          "8249035377",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 16),
-                        ),
-                      ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-          ),
-          Positioned(
-            top: (height - 150) / 2 -
-                kToolbarHeight -
-                kBottomNavigationBarHeight -
-                62,
-            left: (width / 2) - 30,
-            child: CircleAvatar(
-              radius: 30,
-              child: Icon(Icons.person),
-            ),
-          ),
-        ],
+                Positioned(
+                  top: (height - 150) / 2 -
+                      kToolbarHeight -
+                      kBottomNavigationBarHeight -
+                      62,
+                  left: (width / 2) - 30,
+                  child: const CircleAvatar(
+                    radius: 30,
+                    child: Icon(Icons.person),
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
